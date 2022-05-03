ab50ab55c6e07ae00ca309ab4f01388d49d15735
GabrielDCastro <gabrielrangel12@gmail.com>
2022-05-03 17:00:27 -0300

    Service e controllers finalizados (sem DB)

--- src/tasks/shared/task.service.ts
+++ src/tasks/shared/task.service.ts
@@ -1,4 +1,45 @@
+/* eslint-disable @typescript-eslint/no-empty-function */
 import { Injectable } from '@nestjs/common';
+import { Task } from './task';
 
 @Injectable()
-export class TaskService {}
+export class TaskService {
+  
+  tasks: Task[] = [
+    {id:1, description: 'teste', completed: false}
+  ]
+
+  getAll() {
+    return this.tasks;
+  }
+
+  getById(id: number){
+    const task = this.tasks.find((value) => value.id == id);
+    return task;
+  }
+
+  create(task: Task){
+    let lastId = 0;
+    if (this.tasks.length > 0){
+      lastId = this.tasks[this.tasks.length - 1].id;
+    }
+    task.id = lastId + 1;
+    this.tasks.push(task);
+
+    return task;
+  }
+
+  update(task: Task){
+    const TaskArray = this.getById(task.id);
+    if(TaskArray){
+      TaskArray.description = task.description;
+      TaskArray.completed = task.completed;
+    }
+    return TaskArray;
+  }
+
+  delete(id: number){
+    const index = this.tasks.findIndex((value) => value.id == id);
+    this.tasks.splice(index, 1);
+  }
+}
