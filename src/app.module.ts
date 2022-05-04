83a28f7732c85e7d437051f1dcf3e025b386d015
GabrielDCastro <gabrielrangel12@gmail.com>
2022-05-04 18:36:33 -0300

    usando mongo db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TasksModule } from './tasks/tasks.module';
 
 @Module({
-  imports: [TasksModule],
+  imports: [
+    TasksModule,
+    MongooseModule.forRoot('mongodb+srv://user:password@cluster0.1dodq.mongodb.net/test?retryWrites=true&w=majority')
+],
   controllers: [AppController],
   providers: [AppService],
 })
